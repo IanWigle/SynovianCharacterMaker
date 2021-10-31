1ebb1399d05849da7cfc077fb0c7d4e4391cd517
Ian <ianwigle@gmail.com>
2021-10-30 22:55:12 -0400

    Updated Project Solution
    Updated the project solution so that the finished release build does not generate or keep any debug files for the release version.

--- Synovian Character Maker/Properties/AssemblyInfo.cs
+++ Synovian Character Maker/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Synovian Character Maker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Synovian Character Maker")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
